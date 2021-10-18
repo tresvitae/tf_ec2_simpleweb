83a6fa733c08676b7e61de22fe5a939d755ba187
Patryk Futa <patryk.futa@gmail.com>
2021-10-18 13:17:14 +0200

    Modify aws credentials access

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 #VARIABLES
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
 variable "private_key_pair_path" {}
 variable "key_pair_name" {} # key name cannot be created in TF
 variable "region" {
@@ -19,8 +17,7 @@ variable "pubsub2_address" {
 
 # PROVIDERS
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    profile = "default"
     region     = var.region
 }
 
